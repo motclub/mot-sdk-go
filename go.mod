ada1900c345c043d63839314b17f210d28107ae3
Daniel Yin <yinfxs@gmail.com>
2020-07-17 19:42:39 +0800

    feat: 补充心跳上报任务

--- go.mod
+++ go.mod
@@ -6,8 +6,9 @@ require (
 	github.com/go-redis/redis/v8 v8.0.0-beta.6
 	github.com/google/uuid v1.1.1
 	github.com/motclub/common v0.0.0-20200717111949-59920611d82e
+	github.com/robfig/cron/v3 v3.0.1
 	github.com/sirupsen/logrus v1.6.0
-	github.com/smallnest/rpcx v0.0.0-20200714114247-35b07de0def7
+	github.com/smallnest/rpcx v0.0.0-20200714114247-35b07de0def7 // indirect
 )
 
 // replace  github.com/motclub/common => ../common
--- mot/mot.go
+++ mot/mot.go
@@ -7,6 +7,7 @@ import (
 	"github.com/google/uuid"
 	"github.com/motclub/common/caller"
 	"github.com/motclub/common/std"
+	"github.com/robfig/cron/v3"
 	"os"
 	"strings"
 	"time"
@@ -22,25 +23,42 @@ import (
 )
 
 var Client = &client{
+	appId:            "default",
 	instanceId:       uuid.New().String(),
+	cron:             cron.New(cron.WithSeconds()),
 	HeartbeatPeriod:  10 * time.Second,
 	HeartbeatTimeout: 30 * time.Second,
 }
 
 type client struct {
+	appId             string
 	instanceId        string
 	coreServerAddress string
 	moduleCode        string
 	moduleConfig      std.D
 	cache             cache.ICache
 	mq                mq.IMessageQueue
+	cron              *cron.Cron
 
 	HeartbeatPeriod  time.Duration
 	HeartbeatTimeout time.Duration
 }
 
+func (c *client) instanceKey() string {
+	return fmt.Sprintf("mot/%s/%s/%s", c.appId, c.moduleCode, c.instanceId)
+}
+
+func (c *client) runJobs() error {
+	spec := fmt.Sprintf("@every %s", c.HeartbeatPeriod.String())
+	if _, err := c.cron.AddFunc(spec, func() {
+		_ = c.cache.Set(c.instanceKey(), time.Now().Unix(), 3*c.HeartbeatPeriod)
+	}); err != nil {
+		return err
+	}
+	return nil
+}
+
 func New(moduleCode string, coreServerAddress ...string) error {
-	Client.moduleCode = moduleCode
 	// 核心模块地址
 	var server string
 	if v := os.Getenv("MOT_SERVER"); v != "" {
@@ -49,6 +67,13 @@ func New(moduleCode string, coreServerAddress ...string) error {
 		server = coreServerAddress[0]
 	} else {
 		server = "tcp@mot:8889"
+	}
+	if v := os.Getenv("MOT_MODULE_CODE"); v != "" {
+		moduleCode = v
+	}
+	Client.moduleCode = moduleCode
+	if v := os.Getenv("MOT_APP_ID"); v != "" {
+		Client.appId = v
 	}
 	if !strings.Contains(server, "@") && !strings.HasPrefix(server, "http") {
 		server = fmt.Sprintf("tcp@%s", server)
@@ -103,6 +128,9 @@ func New(moduleCode string, coreServerAddress ...string) error {
 		return fmt.Errorf("unsupported message queue adapter: %s", adapter)
 	}
 
+	if err := Client.runJobs(); err != nil {
+		panic(err)
+	}
 	return nil
 }
 
